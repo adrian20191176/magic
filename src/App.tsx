959f746cfd1261844f8e861343e38b2a37ba1167
Adrian Anchelo <adrian.20191176@iit.ac.lk>
2025-09-08 11:07:05 +0530

    Add animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
 import { useState } from "react";
 import "./App.css";
-import { getCard } from "./utils/card_api";
+import { animate } from "animejs";
+import { cards } from "./utils/card_api";
+import _ from "lodash";
 
 function App() {
-  const [isFlipped, setIsFlipped] = useState(true);
+  const [cardValue, setCardValue] = useState<string>("back");
 
-  function onCardTurn() {
-    setIsFlipped((previousValue) => !previousValue);
+  async function onCardTurn() {
+    animate(".magic-text", {
+      opacity: 0,
+      duration: 550,
+      ease: "inOutSine",
+    }).then(() => {
+      animate(".magic-text", {
+        opacity: 1,
+        duration: 550,
+        ease: "inOutSine",
+      });
+    });
+
+    animate(".poker-card", {
+      rotateY: 90,
+      ease: "easeInOutQuad",
+      duration: 550,
+    }).then(async () => {
+      setCardValue((previousValue) =>
+        previousValue !== "back" ? "back" : _.sample(cards) ?? "ah"
+      );
+
+      animate(".poker-card", {
+        rotateY: 0,
+        duration: 550,
+        ease: "easeInOutQuad",
+      });
+    });
   }
 
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-4">
-      <p className="font-epunda text-2xl font-bold">
-        {isFlipped
+      <p className="font-epunda text-2xl font-bold magic-text">
+        {cardValue === "back"
           ? "Could I find your card?"
           : "Ta-da! Here's the card you took."}
       </p>
       <img
-        className="h-72 adrian"
-        src={isFlipped ? `${getCard("back.png")}` : "cards/ac.png"}
+        className="h-72 poker-card"
+        src={`cards/${cardValue}.png`}
         onClick={onCardTurn}
       />
     </div>
